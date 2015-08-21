01753abbc7dd1d83e6e2a2e7f765a5637ce86042
MuhammedAbdullah <MuhammedAbdullah@mag>
2015-08-21 16:33:42 +0300

    RepoService Implementation

--- src/main/java/Model/RepoServiceImp.java
+++ src/main/java/Model/RepoServiceImp.java
@@ -5,10 +5,43 @@
  */
 package Model;
 
+import java.sql.ResultSet;
+import java.util.List;
+
 /**
  *
  * @author MuhammedAbdullah
  */
-public class RepoServiceImp {
+public class RepoServiceImp implements RepoService{
+
+    @Override
+    public ResultSet executeQuery(String query) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<User> getUserList() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User loginUser(String name, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean registerUser(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean banUser(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void rememberPassword(String email) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
